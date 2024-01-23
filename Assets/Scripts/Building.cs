e7544e692c87c11397060bf6d0d2117463a03216
HugoGonzaMora <hugoma21@gmail.com>
2024-01-23 11:41:16 +0100

    sillageimin commit
    :D

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public BuildingPresets preset;
+    public GameObject sillaGeimin;
+
+
+    public void Update()
+    {
+        if(sillaGeimin == null)
+        {
+            sillaGeimin.SetActive(true);
+        }
+        else if(sillaGeimin != null)
+        {
+            sillaGeimin = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Update();
+
+        Debug.Log("Fokiu");
+    }
 }
